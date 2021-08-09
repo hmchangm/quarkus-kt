6e4b7ac9a376c32315a018b6f06872facee1eed0
hmchangm <dickgg@gmail.com>
2021-08-10 07:25:15 +0800

    commit Richard version

--- src/main/kotlin/idv/brandy/repository/FruitRepository.kt
+++ src/main/kotlin/idv/brandy/repository/FruitRepository.kt
@@ -1,5 +1,6 @@
 package idv.brandy.repository
 
+import arrow.core.Either
 import arrow.core.Option
 import idv.brandy.model.Fruit
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase
@@ -7,11 +8,13 @@ import javax.enterprise.context.ApplicationScoped
 
 @ApplicationScoped
 class FruitRepository : PanacheRepositoryBase<Fruit, String> {
-    fun findByUuid(uuid: String): Option<Fruit> =
-        Option.fromNullable(
-            find("uuid", uuid)
-                .firstResultOptional<Fruit>().orElse(null)
-        )
+    fun findByUuid(uuid: String): Either<Throwable, Option<Fruit>> =
+        Either.catch {
+            Option.fromNullable(
+                find("uuid", uuid)
+                    .firstResultOptional<Fruit>().orElse(null)
+            )
+        }
 
     fun findByName(name: String) = find("name", name).firstResult<Fruit>()
 }
--- src/main/kotlin/idv/brandy/service/FruitService.kt
+++ src/main/kotlin/idv/brandy/service/FruitService.kt
@@ -1,37 +1,37 @@
 package idv.brandy.service
 
-import arrow.core.Either
-import arrow.core.None
-import arrow.core.Some
+import arrow.core.*
 import idv.brandy.model.Fruit
 import idv.brandy.repository.FruitRepository
 import javax.enterprise.context.ApplicationScoped
 
 @ApplicationScoped
 class FruitService(val fruitRepository: FruitRepository) {
+    // TODO - mutable list?
+    fun findAll(): Either<Throwable, MutableList<Fruit>> = Either.catch { fruitRepository.findAll().list() }
 
-    fun findAll() = Either.catch {
-        fruitRepository.findAll().list<Fruit>()
-    }
+    fun save(fruit: Fruit): Either<Throwable, Unit> = Either.catch { fruitRepository.persist(fruit) }
 
-    fun save(fruit: Fruit) = Either.catch {
-        fruitRepository.persist(fruit)
-    }
+    fun findByUuid(uuid: String): Either<Throwable, Option<Fruit>> = fruitRepository.findByUuid(uuid)
 
-    fun findByUuid(uuid: String) = Either.catch {
-        fruitRepository.findByUuid(uuid)
-    }
-    fun delete(fruit: Fruit) = Either.catch {
-        fruitRepository.delete(fruit)
-    }
+    fun delete(fruit: Fruit): Either<Throwable, Unit> = Either.catch { fruitRepository.delete(fruit) }
 
-    fun modify(uuid: String, fruit: Fruit) = Either.catch {
-        when (val either = findByUuid(uuid)) {
-            is Either.Left -> throw RuntimeException("data store access error", either.value)
-            is Either.Right -> when (val opt = either.value) {
-                is None -> throw RuntimeException("no this fruit $uuid")
-                is Some -> opt.value.copy(name = fruit.name).let { save(it);it }
+    fun modify(uuid: String, fruit: Fruit): Either<Throwable, Fruit> =
+        findByUuid(uuid).flatMap { x ->
+            x.map { it.copy(name = fruit.name) }.let {
+                when (val f = it) {
+                    is Some -> f.value.let { v -> save(v).map { v } }
+                    is None -> Either.Left(RuntimeException("No fruit with UUID: $uuid found"))
+                }
             }
         }
-    }
+//        Either.catch {
+//        when (val either = findByUuid(uuid)) {
+//            is Either.Left -> throw RuntimeException("data store access error", either.value)
+//            is Either.Right -> when (val opt = either.value) {
+//                is None -> throw RuntimeException("no this fruit $uuid")
+//                is Some -> opt.value.copy(name = fruit.name).let { save(it);it }
+//            }
+//        }
+//    }
 }
